71aac2e4c0e1813340571281464c5663f03c9897
leonardo <leonardo@localhost.localdomain>
2019-05-14 14:20:12 +0200

    First commit

---
+++ apt/start_linux_setup.sh
@@ -0,0 +1,132 @@
+#!/bin/bash
+
+# If it is equal to 0 you are root
+# This script must be run as a normal user
+if [ "$EUID" = 0 ]
+  then echo "You are root. Please run this script as a normal user!"
+  exit 1
+fi
+
+# System update
+echo "Updating your system..."
+sudo apt update -y && sudo apt upgrade -y
+
+############################################
+
+# Balena Etcher: to create Live USB
+echo "Installing Balena Etcher..."
+echo "deb https://deb.etcher.io stable etcher" | sudo tee /etc/apt/sources.list.d/balena-etcher.list
+sudo apt-key adv --keyserver keyserver.ubuntu.com --recv-keys 379CE192D401AB61
+sudo apt-get update
+sudo apt-get install balena-etcher-electron -y
+
+# Chromium: Google's open source browser
+echo "Installing Chromium..."
+sudo apt install -y chromium-browser
+
+# Git: free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
+echo "Installing Git..."
+sudo apt install -y git
+
+# Google Chrome: needs no introduction
+echo "Installing Google Chrome..."
+wget -q -O - https://dl-ssl.google.com/linux/linux_signing_key.pub | sudo apt-key add -
+sudo sh -c 'echo "deb http://dl.google.com/linux/chrome/deb/ stable main" >> /etc/apt/sources.list.d/google-chrome.list'
+sudo apt update && sudo apt upgrade
+sudo apt install -y google-chrome-stable
+
+# Gimp: GNU Image Manipulation Program
+echo "Installing Gimp..."
+sudo apt install -y gimp
+
+# Inkscape: vector edit graphics
+echo "Installing Inkscape..."
+sudo apt install -y inkscape
+
+# Htop: see all active process
+echo "Installing Htop..."
+sudo apt install -y htop
+
+# Nano: small text editor
+echo "Installing Nano..."
+sudo apt install -y nano
+
+# Neofetch: CLI system information tool
+echo "Installing Neofetch..."
+sudo apt install -y neofetch
+
+# Spotify: music player
+echo "Installing Spotify..."
+sudo snap install spotify # using snap
+
+# Telegram: client desktop for Telegram
+echo "Installing Telegram..."
+sudo apt install -y telegram-desktop
+
+# Visual Studio Code: text editor by Microsoft
+echo "Installing Visual Studio Code..."
+sudo snap install --classic code # using snap
+
+############################################
+
+# EXTENSIONS (only for GNOME)
+if [ "$DESKTOP_SESSION" = "gnome" ]
+  # Gnome Tweaks: program that allows you to customize Gnome
+  echo "Installing Gnome Tweaks..."
+  sudo apt install -y gnome-tweaks
+
+  # Enable theme modification
+  sudo apt install -y gnome-shell-extension-user-theme
+  # Install Dash to Dock
+  sudo apt install -y gnome-shell-extension-dashtodock
+
+  # ICONS
+  # Papirus Icon Theme
+  echo "Installing Papirus Icon Theme..."
+  sudo apt install -y papirus-icon-theme
+
+  # THEMES
+  # Materia Theme
+  echo "Installing Materia Theme..."
+  sudo apt install -y materia-gtk-theme
+  # Adapta Theme
+  echo "Installing Adapta Theme..."
+  sudo apt install -y adapta-gtk-theme
+  # Arc Theme
+  echo "Installing Arc Theme..."
+  sudo apt install -y arc-theme
+
+  # CURSORS
+  # Breeze Cursor
+  echo "Installing Breeze Cursor..."
+  sudo apt install -y breeze-cursor-theme
+  # Breeze-Adapta Cursor
+  git clone https://github.com/mustafaozhan/Breeze-Adapta.git && cd Breeze-Adapta && chmod +x install.sh && sh install.sh
+  cd
+
+  # APPLYING
+  # Theme (Materia-dark-compact)
+  gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark-compact'
+  # Cursor (Breeze-Snow)
+  gsettings set org.gnome.desktop.interface cursor-theme 'Breeze-Adapta'
+  # Icon pack (Papirus-Dark)
+  gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+  # Top bar
+  # gsettings set org.gnome.shell.extensions.user-theme name 'Materia-dark-compact'
+fi
+
+# Refresh
+sudo apt update -y && sudo apt upgrade -y
+
+# All programs installed
+echo "Done. All programs installed!"
+
+# Reboot?
+read -p "Do you want reboot? (y/n): " reboot
+if [ $reboot == "y" ]
+then
+	reboot
+else
+	echo "Thank you for using the script!"
+fi
+exit
---
+++ dnf/start_linux_setup.sh
@@ -0,0 +1,144 @@
+#!/bin/bash
+
+# If it is equal to 0 you are root
+# This script must be run as a normal user
+if [ "$EUID" = 0 ]
+  then echo "You are root. Please run this script as a normal user!"
+  exit 1
+fi
+
+# System update
+echo "Updating your system..."
+sudo dnf upgrade -y
+
+# RPM Fusion
+echo "Installing RPM Fusion..."
+sudo dnf install -y https://download1.rpmfusion.org/free/fedora/rpmfusion-free-release-$(rpm -E %fedora).noarch.rpm https://download1.rpmfusion.org/nonfree/fedora/rpmfusion-nonfree-release-$(rpm -E %fedora).noarch.rpm
+
+# Flatpak and Flathub
+echo "Installing Flatpak..."
+sudo dnf install -y flatpak
+flatpak remote-add --if-not-exists flathub https://flathub.org/repo/flathub.flatpakrepo
+
+############################################
+
+# Balena Etcher: to create Live USB
+echo "Installing Balena Etcher..."
+sudo wget https://balena.io/etcher/static/etcher-rpm.repo -O /etc/yum.repos.d/etcher-rpm.repo
+sudo dnf install -y balena-etcher-electron
+
+# Chromium: Google's open source browser
+echo "Installing Chromium..."
+#sudo dnf install -y chromium-browser
+sudo dnf install -y chromium
+
+# Git: free and open source distributed version control system designed to handle everything from small to very large projects with speed and efficiency.
+echo "Installing Git..."
+sudo dnf install -y git
+
+# Google Chrome: needs no introduction
+#echo "Installing Google Chrome..."
+#sudo dnf install -y google-chrome-stable
+
+# Gimp: GNU Image Manipulation Program
+echo "Installing Gimp..."
+sudo dnf install -y gimp
+
+# Inkscape: vector edit graphics
+echo "Installing Inkscape..."
+sudo dnf install -y inkscape
+
+# Htop: see all active process
+echo "Installing Htop..."
+sudo dnf install -y htop
+
+# Nano: small text editor
+echo "Installing Nano..."
+sudo dnf install -y nano
+
+# Neofetch: CLI system information tool
+echo "Installing Neofetch..."
+sudo dnf install -y neofetch
+
+# Spotify: music player
+echo "Installing Spotify..."
+flatpak install -y flathub com.spotify.Client # using flatpak
+
+# Telegram: client desktop for Telegram
+echo "Installing Telegram..."
+sudo dnf install -y telegram-desktop
+
+# Visual Studio Code: text editor by Microsoft
+echo "Installing Visual Studio Code..."
+flatpak install -y flathub com.visualstudio.code # using flatpak
+
+############################################
+
+# EXTENSIONS (only for GNOME)
+if [ "$DESKTOP_SESSION" = "gnome" ]
+  # Gnome Tweaks: program that allows you to customize Gnome
+  echo "Installing Gnome Tweaks..."
+  sudo dnf install -y gnome-tweaks
+
+  # Enable theme modification
+  sudo dnf install -y gnome-shell-extension-user-theme
+  # Install Dash to Dock
+  sudo dnf install -y gnome-shell-extension-dash-to-dock
+
+  # ICONS
+  # Papirus Icon Theme
+  echo "Installing Papirus Icon Theme..."
+  sudo dnf install -y papirus-icon-theme
+
+  # THEMES
+  # Materia Theme
+  echo "Installing Materia Theme..."
+  #sudo dnf install -y materia-theme
+  #flatpak install flathub org.gtk.Gtk3theme.Materia{,-dark,-light}{,-compact}
+  git clone --depth 1 https://github.com/nana-4/materia-theme
+  cd materia-theme
+  sudo ./install.sh
+  cd
+
+  # Adapta Theme
+  echo "Installing Adapta Theme..."
+  sudo dnf install -y adapta-gtk-theme
+  # Arc Theme
+  echo "Installing Arc Theme..."
+  sudo dnf install -y arc-theme
+
+  # CURSORS
+  # Breeze Cursor
+  echo "Installing Breeze Cursor..."
+  sudo dnf install -y breeze-cursor-theme
+  # Breeze-Adapta Cursor
+  git clone https://github.com/mustafaozhan/Breeze-Adapta.git && cd Breeze-Adapta && chmod +x install.sh && sh install.sh
+  cd
+
+  # APPLYING
+  # Theme (Materia-dark-compact)
+  gsettings set org.gnome.desktop.interface gtk-theme 'Materia-dark-compact'
+  #gsettings set org.gnome.desktop.interface gtk-theme 'Arc-Dark'
+  # Cursor (Breeze-Snow)
+  gsettings set org.gnome.desktop.interface cursor-theme 'Breeze-Adapta'
+  # Icon pack (Papirus-Dark)
+  gsettings set org.gnome.desktop.interface icon-theme 'Papirus-Dark'
+  # Top bar
+  # gsettings set org.gnome.shell.extensions.user-theme name 'Materia-dark-compact'
+fi
+
+# Refresh
+sudo dnf upgrade -y
+
+# All programs installed
+echo "Done. All programs installed!"
+
+# Reboot?
+read -p "To apply all changes a reboot is required. Do you want reboot? [y/n]: " reboot
+if [ $reboot == "y" ]
+then
+	reboot
+else
+	echo "Thank you for using the script!"
+fi
+exit
